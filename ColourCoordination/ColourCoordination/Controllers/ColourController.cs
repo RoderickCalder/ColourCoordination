7c5dde01a417491c2fbf6724634d7a48724c1914
Roderick Calder <roderick.calder@zuto.com>
2018-12-04 10:36:08 +0000

    fuck yea boiz

---
+++ ColourCoordination/ColourCoordination/Controllers/ColourController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ColourCoordination.Models;
+
+namespace ColourCoordination.Controllers
+{
+    public class ColourController : Controller
+    {
+        [HttpGet]
+        [Route("set-shirt")]
+        public IActionResult GetSetShirtColor()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("set-shirt")]
+        public IActionResult PostSetShirtColor(Outfit outfit)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("set-pants")]
+        public IActionResult GetSetPantsColor()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("set-pants")]
+        public IActionResult PostSetPantsColor(Outfit outfit)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("set-shoes")]
+        public IActionResult GetSetShoeColor()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("set-shoes")]
+        public IActionResult PostSetShoeColor(Outfit outfit)
+        {
+
+        }
+    }
+
+    public class Outfit
+    {
+        public string ShirtColor { get; set; }
+        public string PantsColor { get; set; }
+        public string ShoeColor { get; set; }
+    }
+
+}
